d278b6e0f7fd7ea6d55a92c56d0ab875e643ed52
Brett Adamson <adamsonb12@gmail.com>
2018-01-16 08:44:25 -0700

    installed needed packages and created some folders. updated app.js to be more customized and plain

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Kenyon, you suuuuuuck</Text>
+      </View>
+    );
+  };
+}
+
+export default App;
