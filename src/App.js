14445db63ee09d87c8c6a512074da2526546c4cc
Brett Adamson <adamsonb12@gmail.com>
2018-01-16 09:02:03 -0700

    more setup done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 class App extends Component {
   render() {
     return (
       <View>
-        <Text>Kenyon, you suuuuuuck</Text>
+        <Text>Kenyon, Happy Birthday</Text>
       </View>
     );
   };
